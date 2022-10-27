6f150fc20ca26c5a2bb46fa5ef6e02c22e6ab2ed
Anthony Re <anthonyjosephre@gmail.com>
2022-10-27 15:17:03 -0400

    Adjusted npm scripts, ts configuration

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const APP_PORT = process.env.PORT || 2305;
+const APP_HOST = process.env.HOST || "127.0.0.1";
+
+/////////////// Middlewares ///////////////
+
+app.use(morgan("dev"));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+/////////////// Routes ///////////////
+
+const itemsRouter = require(path.resolve("items/router"));
+
+app.use("/", itemsRouter);
+
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    console.log(err);
+
+    return err ? res.status(err.status || 500).end() : next();
+  });
+}
+
+app.listen(APP_PORT, APP_HOST, () => {
+  console.log(`App Started at ${APP_HOST}:${APP_PORT}`);
+});

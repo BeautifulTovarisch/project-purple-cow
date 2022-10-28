2032969daac0301f1645c1ca4aff637a1211ecd2
Anthony Re <anthonyjosephre@gmail.com>
2022-10-28 14:40:13 -0400

    Fixed permissions on dev_user; Removing some duplication from dal

--- src/items/dal.ts
+++ src/items/dal.ts
@@ -4,21 +4,23 @@
 import { Knex } from "knex";
 import { Item } from "./model";
 
-const db = require("../config/db");
+import db from "../config/db";
+
+const table = "ppc.items";
 
 /** getItem(id) retrieves Item [id] from the items table.
  * @returns a Promise containing the item or an error. */
 export const getItem = (id: number): Promise<Item> =>
-  db("items").where("id", id).first();
+  db(table).where("id", id).first();
 
 /** getItems() retrieves at all [Item]s from the database.
  * @returns a Promise containing an array of Items or an error. */
-export const getItems = (): Promise<Array<Item>> => db("items");
+export const getItems = (): Promise<Array<Item>> => db(table);
 
 /** updateItem(id, name) updates the name of item [id] to be [name].
  * @returns a Promise containing [item] updated or an error.  */
 export const updateItem = (id: number, name: string): Promise<Item> =>
-  db("items").where("id", id).update({
+  db(table).where("id", id).update({
     name,
   });
 
@@ -26,11 +28,11 @@ export const updateItem = (id: number, name: string): Promise<Item> =>
  * @returns an Promise containing the [id] of the deleted item or
  * an error. */
 export const deleteItem = (id: number): Promise<number> =>
-  db("items").where("id", id).del(["id"]);
+  db(table).where("id", id).del(["id"]);
 
 /** deleteAllItems() removes all items from the database.
  * @returns a Promise containing the number of items deleted or an error. */
-export const deleteAllItems = (): Promise<number> => db("items").del();
+export const deleteAllItems = (): Promise<number> => db(table).del();
 
 /** setItems(items) overwrites the current items in the table to be [items].
  * @returns a Promise containing the number of items inserted or an error. */
@@ -38,8 +40,8 @@ export const setItems = (items: Array<Item>): Promise<number> =>
   db.transaction(async (trx: Knex.Transaction) => {
     try {
       // Delete all existing items and replace with [items]
-      await db("items").truncate().transacting(trx);
-      await db("items").insert(items).transacting(trx);
+      await db(table).truncate().transacting(trx);
+      await db(table).insert(items).transacting(trx);
 
       await trx.commit();
     } catch (e) {

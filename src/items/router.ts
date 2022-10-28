b12a81f67225ede106da1876ea0cbe4c38c0caa7
Anthony Re <anthonyjosephre@gmail.com>
2022-10-27 21:17:09 -0400

    Minor cleanup; fixed typing in server entrypoint

--- src/items/router.ts
+++ src/items/router.ts
@@ -2,10 +2,36 @@
  * This sub-router handles RESTful operations against [Items]. Routes are
  * registered under the /items prefix. */
 
-const Router = require("express").Router();
+import { Router, Request, Response } from "express";
 
-Router.get("/", (req, res) => {
+// Alias route handler definition in order to save a few keystrokes.
+type RouteHandler = (req: Request, res: Response) => void;
+
+const ItemRouter = Router();
+
+// Could expose route handlers for more fine-grained unit testing
+
+const handleGet: RouteHandler = (req, res) => {};
+
+const handleGetAll: RouteHandler = (req, res) => {
   return res.end();
-});
+};
 
-export default Router;
+const handleUpdate: RouteHandler = (req, res) => {};
+
+const handleSetAll: RouteHandler = (req, res) => {};
+
+const handleDelete: RouteHandler = (req, res) => {};
+const handleDeleteAll: RouteHandler = (req, res) => {};
+
+ItemRouter.get("/", handleGetAll);
+ItemRouter.get("/:id", handleGet);
+
+ItemRouter.put("/:id", handleUpdate);
+
+ItemRouter.post("/", handleSetAll);
+
+ItemRouter.delete("/", handleDeleteAll);
+ItemRouter.delete("/:id", handleDelete);
+
+export default ItemRouter;
--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,23 @@
  * Simple express-based RESTful API satisfying basic CRUD operations
  * on Items. Item records are persisted in a PostgreSQL database. */
 
-const express = require("express");
+import path from "path";
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
 
-const APP_PORT = process.env.PORT || 3000;
+const APP_PORT = Number(process.env.PORT) ?? 3000;
 const APP_HOST = process.env.HOST || "127.0.0.1";
 
 // Initialize database before application startup to prevent wasting time
-// in the event the server is unable to connect.
+// in the event the client is unable to initialize.
 require("config/dbconfig.js");
 
 /////////////// Middlewares ///////////////
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
 
 /////////////// Routes ///////////////
 
@@ -27,13 +26,13 @@ const itemsRouter = require(path.resolve("items/router"));
 
 app.use("/", itemsRouter);
 
-if (app.get("env") === "development") {
-  app.use((err, req, res, next) => {
-    console.log(err);
+const errorHandler: express.ErrorRequestHandler = (err, req, res, next) => {
+  console.log(err);
 
-    return err ? res.status(err.status || 500).end() : next();
-  });
-}
+  return err ? res.status(err.status || 500).end() : next();
+};
+
+app.use(errorHandler);
 
 app.listen(APP_PORT, APP_HOST, () => {
   console.log(`App Started at ${APP_HOST}:${APP_PORT}`);

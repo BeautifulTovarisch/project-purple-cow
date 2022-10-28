605825bbaaf564e6406eeb2ed9292c86a00d660d
Anthony Re <anthonyjosephre@gmail.com>
2022-10-28 10:03:47 -0400

    Added transacted operation to set current items

--- src/items/dal.ts
+++ src/items/dal.ts
@@ -1,6 +1,7 @@
 /** Items Data Access Layer
  * This file abstracts over the database operations on [Item] records. */
 
+import { Knex } from "knex";
 import { Item } from "./model";
 
 const db = require("../config/db");
@@ -13,11 +14,6 @@ export const getItem = (id: number): Promise<Item> =>
 /** getItems() retrieves at all [Item]s from the database.
  * @returns a Promise containing an array of Items or an error. */
 export const getItems = (): Promise<Array<Item>> => db("items");
-
-/** setItems(items) sets the current items in the table to [items].
- * @returns a Promise containing the number of items inserted or an error. */
-export const setItems = (items: Array<Item>): Promise<number> =>
-  db("items").del();
 
 /** updateItem(id, name) updates the name of item [id] to be [name].
  * @returns a Promise containing [item] updated or an error.  */
@@ -35,3 +31,18 @@ export const deleteItem = (id: number): Promise<number> =>
 /** deleteAllItems() removes all items from the database.
  * @returns a Promise containing the number of items deleted or an error. */
 export const deleteAllItems = (): Promise<number> => db("items").del();
+
+/** setItems(items) overwrites the current items in the table to be [items].
+ * @returns a Promise containing the number of items inserted or an error. */
+export const setItems = (items: Array<Item>): Promise<number> =>
+  db.transaction(async (trx: Knex.Transaction) => {
+    try {
+      // Delete all existing items and replace with [items]
+      await db("items").truncate().transacting(trx);
+      await db("items").insert(items).transacting(trx);
+
+      await trx.commit();
+    } catch (e) {
+      await trx.rollback();
+    }
+  });
